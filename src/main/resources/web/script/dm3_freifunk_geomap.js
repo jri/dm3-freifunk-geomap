d36ab543ca265f6576ea637b64c07da6684f3594
Jörg Richter <jri@deepamehta.de>
2010-10-21 23:25:54 +0200

    Google geocoder added.

--- src/main/resources/web/script/dm3_freifunk_geomap.js
+++ src/main/resources/web/script/dm3_freifunk_geomap.js
@@ -2,23 +2,63 @@ function dm3_freifunk_geomap() {
 
     dm3c.css_stylesheet("/net.freifunk.dm3-freifunk-geomap/script/vendor/openlayers/theme/default/style.css")
 
+    dm3c.javascript_source("/net.freifunk.dm3-freifunk-geomap/script/geomap_renderer.js")
     dm3c.javascript_source("/net.freifunk.dm3-freifunk-geomap/script/vendor/openlayers/OpenLayers.js")
     dm3c.javascript_source("http://maps.google.com/maps/api/js?sensor=false&callback=dm3_freifunk_geomap.init_renderer")
 
-    // ------------------------------------------------------------------------------------------------ Overriding Hooks
+    this.geomap = new GeoMapRenderer()
+    var self = this
+
+    // ------------------------------------------------------------------------------------------------------ Public API
+
+
+
+    // *******************************
+    // *** Overriding Plugin Hooks ***
+    // *******************************
+
+
 
     this.get_canvas_renderer = function() {
-        return dm3_freifunk_geomap.geomap_renderer
+        return this.geomap
+    }
+
+    this.post_update_topic = function(topic, old_properties) {
+        if (topic.type_uri == "net/freifunk/topictype/freikarte") {
+            var street      = topic.properties["net/freifunk/property/street"]
+            var city        = topic.properties["net/freifunk/property/city"]
+            var postal_code = topic.properties["net/freifunk/property/postal_code"]
+            var old_street      = old_properties["net/freifunk/property/street"]
+            var old_city        = old_properties["net/freifunk/property/city"]
+            var old_postal_code = old_properties["net/freifunk/property/postal_code"]
+            var street_changed = old_street != street
+            var city_changed = old_city != city
+            var postal_code_changed = old_postal_code != postal_code
+            /* alert("street: \"" + old_street + "\" => \"" + street + "\" (" + street_changed + ")\n" +
+            "city: \"" + old_city + "\" => \"" + city + "\" (" + city_changed + ")\n" +
+            "postal_code: \"" + old_postal_code + "\" => \"" + postal_code + "\" (" + postal_code_changed + ")") */
+            //
+            if (street_changed || city_changed || postal_code_changed) {
+                var address = street + ", " + postal_code + " " + city
+                this.geomap.geocode(address, update_marker)
+            }
+        }
     }
 
     // ----------------------------------------------------------------------------------------------- Private Functions
 
+    function update_marker(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var location = results[0].geometry.location
+            alert("Geocoder was successful!\n\n" + results[0].formatted_address +
+                "\nlong=" + location.c + "\nlat=" + location.b)
+            self.geomap.add_access_point({long: location.c, lat: location.b})
+        } else {
+            alert("Geocoder was not successful: " + status)
+        }
+    }
 }
 
-dm3c.javascript_source("/net.freifunk.dm3-freifunk-geomap/script/geomap_renderer.js")
-
-dm3_freifunk_geomap.geomap_renderer = new GeoMapRenderer()
-
 dm3_freifunk_geomap.init_renderer = function() {
-    dm3_freifunk_geomap.geomap_renderer.init()
+    dm3c.get_plugin("dm3_freifunk_geomap").geomap.init()
 }
--- src/main/resources/web/script/geomap_renderer.js
+++ src/main/resources/web/script/geomap_renderer.js
@@ -5,6 +5,11 @@ function GeoMapRenderer() {
     this.superclass = TopicmapRenderer
     this.superclass()
 
+    var transform
+    var markers
+    var geocoder
+    var icon
+
     // ------------------------------------------------------------------------------------------------------ Public API
 
     this.init = function() {
@@ -15,38 +20,60 @@ function GeoMapRenderer() {
         //
         var map = new OpenLayers.Map("canvas");
         //
+        transform = get_transformer(new OpenLayers.Projection("EPSG:4326"))     // EPSG:4326 is *long/lat* projection
+        //
         var osm = new OpenLayers.Layer.OSM("OpenSteetMap");
         var gmap = new OpenLayers.Layer.Google("Google Maps");
-        var markers = new OpenLayers.Layer.Markers("Access Points");
+        markers = new OpenLayers.Layer.Markers("Access Points");
         // Note: the marker layer must be added to the map *before* markers are added to it.
         // Otherwise the maps projection isn't propagated and all markers appear at 0/0.
         map.addLayers([gmap, osm, markers]);
         map.addControl(new OpenLayers.Control.LayerSwitcher());
-        map.setCenter(project(10, 51), 6);
+        map.setCenter(transform(10, 51), 6);
         //
         // --- add markers ---
         var size = new OpenLayers.Size(21, 25);
         var offset = new OpenLayers.Pixel(-size.w / 2, -size.h);
-        var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+        icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
         //
-        markers.addMarker(new OpenLayers.Marker(project(13, 52), icon));
+        markers.addMarker(new OpenLayers.Marker(transform(13, 52), icon));
         // add 2nd marker
         // Note: you should not share icons between markers. Clone them instead.
-        markers.addMarker(new OpenLayers.Marker(project(10, 51), icon.clone()));
+        markers.addMarker(new OpenLayers.Marker(transform(10, 51), icon.clone()));
+        //
+        geocoder = new google.maps.Geocoder()
 
-        // Transforms lon/lat into meters.
-        function project(lon, lat) {
-            return new OpenLayers.LonLat(lon, lat).transform(
-                new OpenLayers.Projection("EPSG:4326"),
-                map.getProjectionObject()
-            )
+        // Returns a function that transforms coordinates of the given projection into projection of this map.
+        // (This way the projection object is created only once and is not visible outside.)
+        function get_transformer(projection) {
+            return function(lon, lat) {
+                return new OpenLayers.LonLat(lon, lat).transform(
+                    projection, map.getProjectionObject()
+                )
+            }
         }
     }
 
-    // === Overriding TopicmapRenderer Adapter Methods ===
+    this.geocode = function(address, callback) {
+        geocoder.geocode({address: address}, callback)
+    }
+
+    this.add_access_point = function(pos) {
+        markers.addMarker(new OpenLayers.Marker(transform(pos.long, pos.lat), icon.clone()));
+    }
+
+
+
+    // ***************************************************
+    // *** Overriding TopicmapRenderer Adapter Methods ***
+    // ***************************************************
+
+
 
     this.resize = function() {
         // alert("GeoMapRenderer.resize(): width=" + this.canvas_width + " height=" + this.canvas_height)
         $("#canvas").width(this.canvas_width).height(this.canvas_height)
     }
+
+    // ----------------------------------------------------------------------------------------------- Private Functions
 }
